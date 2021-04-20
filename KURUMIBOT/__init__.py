131093f4b77113932a6acae46bceb3d76de83882
Hirojazz <79592216+Hirojazz@users.noreply.github.com>
2021-04-20 14:00:31 +0530

    Update __init__.py

--- KURUMIBOT/__init__.py
+++ KURUMIBOT/__init__.py
@@ -187,8 +187,8 @@ else:
 
 
 updater = tg.Updater(TOKEN, workers=WORKERS, use_context=True)
-telethn = TelegramClient("yone", API_ID, API_HASH)
-pbot = Client("yonepbot", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
+telethn = TelegramClient("kurumi", API_ID, API_HASH)
+pbot = Client("kurumipbot", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
 dispatcher = updater.dispatcher
 
 DRAGONS = list(DRAGONS) + list(DEV_USERS)
