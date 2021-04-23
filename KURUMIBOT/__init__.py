debc407a8e971e1ba6cf97a8d06e44ed861ec61f
Hirojazz <79592216+Hirojazz@users.noreply.github.com>
2021-04-23 18:14:24 +0530

    Update __init__.py

--- KURUMIBOT/__init__.py
+++ KURUMIBOT/__init__.py
@@ -70,7 +70,7 @@ if ENV:
     API_ID = os.environ.get("API_ID", None)
     API_HASH = os.environ.get("API_HASH", None)
     BOT_ID = int(os.environ.get("BOT_ID", None))
-    ARQ_API = os.environ.get("ARQ_API_BASE_URL, None) 
+    ARQ_API = os.environ.get("ARQ_API_BASE_URL", None) 
     DB_URI = os.environ.get("DATABASE_URL")
     MONGO_DB_URI = os.environ.get("MONGO_DB_URI", None)
     DONATION_LINK = os.environ.get("DONATION_LINK")
