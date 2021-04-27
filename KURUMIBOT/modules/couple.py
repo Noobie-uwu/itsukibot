b65a4b2b113652e0465ead3426b054c8e898b426
Hirojazz <79592216+Hirojazz@users.noreply.github.com>
2021-04-27 21:02:34 +0530

    Update couple.py

--- KURUMIBOT/modules/couple.py
+++ KURUMIBOT/modules/couple.py
@@ -80,3 +80,5 @@ __New couple of the day may be chosen at 12AM {tomorrow}__"""
         print(e)
         await message.reply_text(e)
 
+mod_name = "Couple"
+help = "/couples - To Choose Couple Of The Day"
