fe35155ae3c876469629456d981f6d0043a01592
Hirojazz <79592216+Hirojazz@users.noreply.github.com>
2021-04-27 21:20:46 +0530

    Update chatbot.py

--- KURUMIBOT/modules/chatbot.py
+++ KURUMIBOT/modules/chatbot.py
@@ -57,6 +57,6 @@ async def chatbot_talk(_, message):
     response = luna.response
     await message.reply_text(response)
 
-mod_name = "ChatBot"
-help = """
+__mod_name__ = "ChatBot"
+__help__ = """
 /chatbot [ON|OFF] To Enable Or Disable ChatBot In Your Chat."""
