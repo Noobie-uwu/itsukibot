9384453307620aefbf218e506a3a46441dbb0766
Noobie-uwu <83952718+Noobie-uwu@users.noreply.github.com>
2021-05-10 22:22:30 +0530

    Create Animate.py

---
+++ KURUMIBOT/modules/Animate.py
@@ -0,0 +1,19 @@
+from telethon import *
+from telethon.tl import functions
+from telethon.tl.types import *
+
+from KURUMIBOT import *
+from KURUMIBOT.events import register
+
+@register(pattern="^/animate (.*)")
+async def stickerizer(event):
+
+    newtext = event.pattern_match.group(1)
+    animus = [20, 32, 33, 40, 41, 42, 58]
+    sticcers = await ubot.inline_query(
+        "stickerizerbot", f"#{random.choice(animus)}{(deEmojify(newtext))}"
+    )
+    null = await sticcers[0].download_media(TEMP_DOWNLOAD_DIRECTORY)
+    bara = str(null)
+    await event.client.send_file(event.chat_id, bara, reply_to=event.id)
+    os.remove(bara)
