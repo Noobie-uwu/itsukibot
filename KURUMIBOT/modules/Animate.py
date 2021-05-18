69cd1f09275651cec438d34bdc318f0461deeba3
Hirojazz <79592216+Hirojazz@users.noreply.github.com>
2021-05-19 01:13:21 +0530

    Update Animate.py

--- KURUMIBOT/modules/Animate.py
+++ KURUMIBOT/modules/Animate.py
@@ -3,13 +3,12 @@ import json
 import os
 import random
 import re
-import emoji
+import deemojify
 from telethon import *
 from telethon.tl import functions
 from telethon.tl.types import *
 
 from KURUMIBOT import *
-from KURUMIBOT import TEMP_DOWNLOAD_DIRECTORY
 from KURUMIBOT.events import register
 from KURUMIBOT.ubot import ubot
 
@@ -19,7 +18,7 @@ async def stickerizer(event):
     newtext = event.pattern_match.group(1)
     animus = [20, 32, 33, 40, 41, 42, 58]
     sticcers = await ubot.inline_query(
-        "stickerizerbot", f"#{random.choice(animus)}"
+        "stickerizerbot", f"#{random.choice(animus)}{(deEmojify(newtext))}"
     )
     null = await sticcers[0].download_media(TEMP_DOWNLOAD_DIRECTORY)
     bara = str(null)
