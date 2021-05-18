0b8b7f97216767ab2221fa39b266a66fc935eccc
Hirojazz <79592216+Hirojazz@users.noreply.github.com>
2021-05-18 22:11:49 +0530

    Update __main__.py

--- KURUMIBOT/__main__.py
+++ KURUMIBOT/__main__.py
@@ -19,6 +19,7 @@ from KURUMIBOT import (
     dispatcher,
     StartTime,
     telethn,
+    pbot, 
     updater,
 )
 
@@ -739,4 +740,5 @@ def main():
 if __name__ == "__main__":
     LOGGER.info("Successfully loaded modules: " + str(ALL_MODULES))
     telethn.start(bot_token=TOKEN)
+    pbot.start() 
     main()
