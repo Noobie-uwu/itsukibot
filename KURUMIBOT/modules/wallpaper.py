15dbaa9c9aa88aec451f6b4916ab6b091e1657e1
Hirojazz <79592216+Hirojazz@users.noreply.github.com>
2021-05-18 23:27:35 +0530

    Update wallpaper.py

--- KURUMIBOT/modules/wallpaper.py
+++ KURUMIBOT/modules/wallpaper.py
@@ -1,65 +1,57 @@
-""" Wallpaper Module """
+from random import randint
 
-import os
-import wget
-import shutil
-import requests
+import requests as r
+from KURUMIBOT import SUPPORT_CHAT, WALL_API, dispatcher
+from KURUMIBOT.modules.disable import DisableAbleCommandHandler
+from telegram import Update
+from telegram.ext import CallbackContext, run_async
 
-from pyrogram.types import InputMediaPhoto, InputMediaDocument
-from PIL import Image
-
-from KURUMIBOT.events import register
+# Wallpapers module by @TheRealPhoenix using wall.alphacoders.com
 
 
-@register(pattern="^/wall ?(.*)")
-async def wall(event):
-
-    if os.path.exists("wallpapers/"):
-        shutil.rmtree("wallpapers/", ignore_errors=True)
-
-    limit = min(int(event.flags.get('-l', 8)), 10)
-
-    if msg.filtered_input_str:
-        qu = event.filtered_input_str
-        await event.reply(f"`Seraching Wallpapers for {qu}`")
-        results = requests.get(
-            "https://api.unsplash.com/search/"
-            f"photos?client_id=HWlOs9dNZIbYEkjp87fiEzC9rmE6rKM64tBqXBOLzu8&query={qu}"
-        )
-
-        if results.status_code != 200:
-            return await msg.reply('**Result Not Found**')
-        _json = results.json()['results']
-        if len(_json) < limit:
-            limit = len(_json)
-
-        ss = []
-        os.mkdir("wallpapers")
-
-        for i in range(limit):
-            img = f"wallpapers/wall_{i+1}.png"
-
-            if '-doc' in msg.flags:
-                await pool.run_in_thread(wget.download)(_json[i]['urls']['raw'], img)
-                ss.append(InputMediaDocument(str(img)))
-                continue
-
-            await pool.run_in_thread(wget.download)(_json[i]['urls']['thumb'], img)
-            image = Image.open(img)
-            if not (image.height <= 1280 and image.width <= 1280):
-                image.thumbnail((1280, 1280), Image.ANTIALIAS)
-                a_dex = image.mode.find("A")
-                if a_dex != -1:
-                    new_im = Image.new('RGB', image.size, (255, 255, 255))
-                    new_im.paste(image, mask=image.split()[a_dex])
-                    new_im.save(img, 'JPEG', optimize=True)
-            ss.append(InputMediaPhoto(str(img)))
-
-        await event.reply_chat_action(
-            "upload_photo" if '-doc' not in msg.flags else "upload_document")
-        await event.reply_media_group(ss, True)
-        shutil.rmtree("wallpapers/", ignore_errors=True)
-        await event.delete()
+@run_async
+def wall(update: Update, context: CallbackContext):
+    chat_id = update.effective_chat.id
+    msg = update.effective_message
+    args = context.args
+    msg_id = update.effective_message.message_id
+    bot = context.bot
+    query = " ".join(args)
+    if not query:
+        msg.reply_text("Please enter a query!")
+        return
     else:
-        await event.reply('**Give me Something to search.**')
-        await event.reply_sticker('CAADAQADmQADTusQR6fPCVZ3EhDoFgQ')
+        caption = query
+        term = query.replace(" ", "%20")
+        json_rep = r.get(
+            f"https://api.unsplash.com/search/"
+        ).json()
+        if not json_rep.get("success"):
+            msg.reply_text(f"An error occurred! Report this @{SUPPORT_CHAT}")
+        else:
+            wallpapers = json_rep.get("wallpapers")
+            if not wallpapers:
+                msg.reply_text("No results found! Refine your search.")
+                return
+            else:
+                index = randint(0, len(wallpapers) - 1)  # Choose random index
+                wallpaper = wallpapers[index]
+                wallpaper = wallpaper.get("url_image")
+                wallpaper = wallpaper.replace("\\", "")
+                bot.send_photo(
+                    chat_id,
+                    photo=wallpaper,
+                    caption='Preview',
+                    reply_to_message_id=msg_id,
+                    timeout=60)
+                bot.send_document(
+                    chat_id,
+                    document=wallpaper,
+                    filename='wallpaper',
+                    caption=caption,
+                    reply_to_message_id=msg_id,
+                    timeout=60)
+
+
+WALLPAPER_HANDLER = DisableAbleCommandHandler("wall", wall)
+dispatcher.add_handler(WALLPAPER_HANDLER)
